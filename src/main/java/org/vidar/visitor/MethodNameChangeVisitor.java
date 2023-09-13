e1e3e17d9a5fc9171230728cabecedaf5f42cad7
陈再 <2336485988@qq.com>
2023-09-13 11:43:18 +0800

    paramter混淆

--- src/main/java/org/vidar/visitor/MethodNameChangeVisitor.java
+++ src/main/java/org/vidar/visitor/MethodNameChangeVisitor.java
@@ -3,11 +3,15 @@ package org.vidar.visitor;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.MethodReferenceExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import org.vidar.utils.NameUtils;
 import org.vidar.utils.TransformUtil;
 
 import java.util.Map;
@@ -16,6 +20,32 @@ import java.util.Optional;
 public class MethodNameChangeVisitor extends VoidVisitorAdapter<String[]> {
     @Override
     public void visit(MethodDeclaration md, String[] arg) {
+
+        /* 修改方法参数 */
+        // 获取方法的参数列表
+        for (Parameter parameter : md.getParameters()) {
+            String oldName = parameter.getName().getIdentifier();
+            String newName = NameUtils.generateLocalVariableName();
+            // 修改参数名称
+            parameter.setName(newName);
+
+            // 遍历方法内的语句
+            md.getBody().ifPresent(body -> {
+                for (Statement statement : body.getStatements()) {
+                    // 查找使用到参数的地方
+                    statement.findAll(NameExpr.class).forEach(nameExpr -> {
+                        if (nameExpr.getNameAsString().equals(oldName)) {
+                            // 修改使用到参数的地方的名称
+                            nameExpr.setName(newName);
+                        }
+                    });
+                }
+            });
+        }
+
+
+
+        /* 修改方法名 */
         // 检查注解列表中是否包含 @Override 注解
         boolean hasOverrideAnnotation = md.getAnnotations().stream()
                 .anyMatch(annotation -> annotation.getNameAsString().equals("Override"));
@@ -67,7 +97,7 @@ public class MethodNameChangeVisitor extends VoidVisitorAdapter<String[]> {
                 mc.setName(arg[1]);
             }
         }
-        super.visit(mc,arg);
+        super.visit(mc, arg);
     }
 
 }
